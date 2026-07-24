4025d803b90dbff43426c140beb6f8cb8e50e77c
Stenio <stenioferreira29@gmail.com>
2026-07-24 15:35:03 -0300

    configure prisma 7 client engine

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-const prisma = new PrismaClient();
-
+const prisma = new PrismaClient({
+  adapter,
+});
 
 export default prisma;
