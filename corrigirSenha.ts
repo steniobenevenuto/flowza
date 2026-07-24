02ecccaca42cb10f807f3ba582060da0c9ba21fc
Stenio <stenioferreira29@gmail.com>
2026-07-24 15:26:04 -0300

    remove remaining sqlite prisma

--- corrigirSenha.ts
+++ corrigirSenha.ts
@@ -1,28 +1,52 @@
-import { PrismaClient } from "./src/generated/prisma/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 
-const prisma = new PrismaClient({
-  adapter: new PrismaLibSql({
-    url: "file:./prisma/bot.sqlite"
-  })
-});
+
+const prisma = new PrismaClient();
+
+
 
 async function main() {
-  const hash = await bcrypt.hash("123456", 10);
+
+  const novaSenha = "123456";
+
+
+  const senhaHash = await bcrypt.hash(
+    novaSenha,
+    10
+  );
+
 
   await prisma.usuario.update({
-    where: {
-      email: "teste@teste.com"
+
+    where:{
+      email:"teste@teste.com"
     },
-    data: {
-      senha: hash
+
+    data:{
+      senha:senhaHash
     }
+
   });
 
+
   console.log("Senha atualizada!");
+
+
 }
 
-main().finally(async () => {
+
+
+main()
+.then(async()=>{
+
   await prisma.$disconnect();
+
+})
+.catch(async(e)=>{
+
+  console.log(e);
+
+  await prisma.$disconnect();
+
 });
